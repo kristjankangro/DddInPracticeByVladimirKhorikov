7c295323d9f54b1100294b9e5b1a3234cc4e26e2
kristjan kangro <kristjan.kangro@visma.com>
2025-02-11 22:59:13 +0200

    money tests done

--- Logic/Money.cs
+++ Logic/Money.cs
@@ -3,12 +3,19 @@ namespace Logic;
 //Value Object
 public class Money : ValueObject<Money>
 {
-	public int Cents1Count { get; private set; }
-	public int Cents10Count { get; private set; }
-	public int Cents25Count { get; private set; }
-	public int Dollar1Count { get; private set; }
-	public int Dollar5Count { get; private set; }
-	public int Dollar20Count { get; private set; }
+	public int Cents1Count { get; }
+	public int Cents10Count { get; }
+	public int Cents25Count { get; }
+	public int Dollar1Count { get; }
+	public int Dollar5Count { get; }
+	public int Dollar20Count { get; }
+
+	public decimal Amount => Cents1Count * 0.01m +
+	                        Cents10Count * 0.1m
+	                        + Cents25Count * 0.25m
+	                        + Dollar1Count
+	                        + Dollar5Count * 5
+	                        + Dollar20Count * 20;
 
 	public Money(
 		int cents1Count,
@@ -18,6 +25,13 @@ public class Money : ValueObject<Money>
 		int dollar5Count,
 		int dollar20Count)
 	{
+		if (cents1Count < 0) throw new InvalidOperationException();
+		if (cents10Count < 0) throw new InvalidOperationException();
+		if (cents25Count < 0) throw new InvalidOperationException();
+		if (dollar1Count < 0) throw new InvalidOperationException();
+		if (dollar5Count < 0) throw new InvalidOperationException();
+		if (dollar20Count < 0) throw new InvalidOperationException();
+
 		Cents1Count += cents1Count;
 		Cents10Count += cents10Count;
 		Cents25Count += cents25Count;
@@ -37,14 +51,25 @@ public class Money : ValueObject<Money>
 			left.Dollar20Count + right.Dollar20Count);
 	}
 
+	public static Money operator -(Money left, Money right)
+	{
+		return new Money(
+			left.Cents1Count - right.Cents1Count,
+			left.Cents10Count - right.Cents10Count,
+			left.Cents25Count - right.Cents25Count,
+			left.Dollar1Count - right.Dollar1Count,
+			left.Dollar5Count - right.Dollar5Count,
+			left.Dollar20Count - right.Dollar20Count);
+	}
+
 	protected override bool EqualsCore(Money other)
 	{
 		return Cents1Count == other.Cents1Count
-			&& Cents10Count == other.Cents10Count
-			&& Cents25Count == other.Cents25Count
-			&& Dollar1Count == other.Dollar1Count
-			&& Dollar5Count == other.Dollar5Count
-			&& Dollar20Count == other.Dollar20Count;
+		       && Cents10Count == other.Cents10Count
+		       && Cents25Count == other.Cents25Count
+		       && Dollar1Count == other.Dollar1Count
+		       && Dollar5Count == other.Dollar5Count
+		       && Dollar20Count == other.Dollar20Count;
 	}
 
 	protected override int GetHashCodeCore()
@@ -59,6 +84,5 @@ public class Money : ValueObject<Money>
 			hashCode = (hashCode * 397) ^ Dollar20Count;
 			return hashCode;
 		}
-		
 	}
 }
--- Tests/MoneySpecs.cs
+++ Tests/MoneySpecs.cs
@@ -11,6 +11,7 @@ public class MoneySpecs
 	{
 		var money1 = new Money(1, 1, 1, 1, 1, 1);
 		var money2 = new Money(1, 1, 1, 1, 1, 1);
+
 		var sum = money1 + money2;
 
 		sum.Cents1Count.Should().Be(2);
@@ -19,5 +20,98 @@ public class MoneySpecs
 		sum.Dollar1Count.Should().Be(2);
 		sum.Dollar5Count.Should().Be(2);
 		sum.Dollar20Count.Should().Be(2);
+	}
+
+	[Fact]
+	public void Two_money_instances_are_equal_if_same_amounts()
+	{
+		var money1 = new Money(1, 1, 1, 1, 1, 1);
+		var money2 = new Money(1, 1, 1, 1, 1, 1);
+
+		money1.Should().Be(money2);
+		money1.GetHashCode().Should().Be(money2.GetHashCode());
+	}
+
+	[Fact]
+	public void Two_money_instances_are_not_equal_if_different_amounts()
+	{
+		var cents100 = new Money(100, 0, 0, 0, 0, 0);
+		var dollar = new Money(0, 0, 0, 1, 0, 0);
+
+		cents100.Should().NotBe(dollar);
+		cents100.GetHashCode().Should().NotBe(dollar.GetHashCode());
+	}
+
+	[Theory]
+	[InlineData(-1, 0, 0, 0, 0, 0)]
+	[InlineData(0, -1, 0, 0, 0, 0)]
+	[InlineData(0, 0, -1, 0, 0, 0)]
+	[InlineData(0, 0, 0, -1, 0, 0)]
+	[InlineData(0, 0, 0, 0, -1, 0)]
+	[InlineData(0, 0, 0, 0, 0, -1)]
+	public void Cannot_create_negative_money(
+		int cents1,
+		int cents10,
+		int cents25,
+		int dollar1,
+		int dollar5,
+		int dollar20
+	)
+	{
+		Action action = () => new Money(cents1, cents10, cents25, dollar1, dollar5, dollar20);
+
+		action.Should().Throw<InvalidOperationException>();
+	}
+
+	[Theory]
+	[InlineData(0, 0, 0, 0, 0, 0, 0)]
+	[InlineData(1, 0, 0, 0, 0, 0, 0.01)]
+	[InlineData(1, 1, 0, 0, 0, 0, 0.11)]
+	[InlineData(1, 1, 1, 0, 0, 0, 0.36)]
+	[InlineData(1, 1, 1, 1, 0, 0, 1.36)]
+	[InlineData(1, 1, 1, 1, 1, 0, 6.36)]
+	[InlineData(1, 1, 1, 1, 1, 1, 26.36)]
+	public void Amount_is_calculated_correctly(
+		int cents1,
+		int cents10,
+		int cents25,
+		int dollar1,
+		int dollar5,
+		int dollar20,
+		decimal expected)
+	{
+		
+		new Money(cents1, cents10, cents25, dollar1, dollar5, dollar20).Amount.Should().Be(expected);
+		
+	}
+
+	[Fact]
+	public void Substraction_of_money_produces_correct_amount()
+	{
+		var money1 = new Money(10, 10, 10, 10, 10, 10);
+		var money2 = new Money(1, 1, 1, 1, 1, 1);
+
+		Money result = money1 - money2;
+		
+		result.Cents1Count.Should().Be(9);
+		result.Cents10Count.Should().Be(9);
+		result.Cents25Count.Should().Be(9);
+		result.Dollar1Count.Should().Be(9);
+		result.Dollar5Count.Should().Be(9);
+		result.Dollar20Count.Should().Be(9);
+	}
+	
+	[Fact]
+	public void Cannot_substract_more_than_exists()
+	{
+		var money1 = new Money(1, 1, 1, 1, 1, 1);
+		var money2 = new Money(10, 10, 10, 10, 10, 10);
+
+		Action action = () =>
+		{
+			var money = money1 - money2;
+		};
+		
+		action.Should().Throw<InvalidOperationException>();
 	}
 }
